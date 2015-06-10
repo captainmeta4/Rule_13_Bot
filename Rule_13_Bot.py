e5ad8cda0fbcd140909ab6acfa8e1ba2e3a56d4d
captainmeta4 <captainmeta4@users.noreply.github.com>
2015-06-10 15:47:07 -0400

    Update Rule_13_Bot.py

--- Rule_13_Bot.py
+++ Rule_13_Bot.py
@@ -39,7 +39,7 @@ class Bot():
             #ignore posts that have already been approved by another mod
             #Prevents repeat behavior on any false positive when heroku restarts bot once per day
             #Thanks to /u/coupdetaco for finding this behavior
-            if submisison.approved_by != None:
+            if submission.approved_by != None:
                 continue
 
             #Get the submission url
